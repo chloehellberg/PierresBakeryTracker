223d6031a498d200cb68ba00c0bf5aa8f72c33b8
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-02 09:39:31 -0700

    Added /vendors path to display allVendors - displaying correctly although currently there are no vendors added yet and no form has been created. That's up next - create add new vendor form and post/path.

---
+++ BakeryTracker/Controllers/VendorsController.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using BakeryTracker.Models;
+
+namespace BakeryTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+
+
+  }
+}
