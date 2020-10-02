d3d3671332fc4073300b069fef492fefdd9be435
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-02 12:19:16 -0700

    Found where the link was broken for the order details. Now when you click the title of the order you'll be able to see the description, price and date for each order.

--- BakeryTracker/Controllers/VendorsController.cs
+++ BakeryTracker/Controllers/VendorsController.cs
@@ -40,11 +40,12 @@ namespace BakeryTracker.Controllers
     }
 
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderTitle)
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string stringPrice, string orderDate)
     {
+      int orderPrice = int.Parse(stringPrice);
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderTitle);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
