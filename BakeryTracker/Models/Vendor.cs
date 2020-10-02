0dfbebb2269a317f4485291c8107165aed70a6ee
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-02 10:05:01 -0700

    Add vendor description property and form field.

--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -6,21 +6,23 @@ namespace BakeryTracker.Models
   {
     private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
+    public string Description { get; set; }
     public int Id { get; }
     // public List<Order> Orders { get; set; }
 
-    public Vendor(string vendorName)
+    public Vendor(string vendorName, string vendorDescription)
     {
       Name = vendorName;
+      Description = vendorDescription;
       _instances.Add(this);
       Id = _instances.Count;
       // Orders = new List<Order>{};
     }
 
-    // public static void ClearAll()
-    // {
-    //   _instances.Clear();
-    // }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
 
     public static List<Vendor> GetAll()
     {
