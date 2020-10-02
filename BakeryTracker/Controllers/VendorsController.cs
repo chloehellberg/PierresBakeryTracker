609c4685590ff5bfd64c57a6f4d6327fb8ff125e
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-02 09:57:58 -0700

    Functionality added so that Vendors can be added to Vendor List.

--- BakeryTracker/Controllers/VendorsController.cs
+++ BakeryTracker/Controllers/VendorsController.cs
@@ -15,7 +15,18 @@ namespace BakeryTracker.Controllers
       return View(allVendors);
     }
 
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
 
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
 
   }
 }
