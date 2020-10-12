deff49ead12267430518270cef521c1b5185bace
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-11 17:35:52 -0700

    Add two GetAll() method tests on Order.

--- BakeryTracker.Tests/ModelTests/OrderTests.cs
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -45,5 +45,40 @@ namespace BakeryTracker.Tests
 
       Assert.AreEqual(60, newOrder.GetCost());
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> { };
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnOrders_OrderList()
+    {
+       string title01 = "Dinner Rolls";
+      string title02 = "Croissants";
+      string description01 = "fluffy";
+      string description02 = "toasted";
+      int amount01 = 1;
+      int price01 = 5;
+      int price02 = 3;
+      string date01 = "June 5th";
+      string date02 = "Oct 5th";
+      Order newOrder1 = new Order(title01, description01, amount01, price01, date01);
+      Order newOrder2 = new Order(title02, description02, amount01, price02, date02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
+
+
+
+
